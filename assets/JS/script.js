0e4b790fc4ccbbe090e5ec1bfd327931fb675118
AngMaher <angelamaher@mail.com>
2022-10-21 14:54:26 +0000

    set up structure for games function

---
+++ assets/JS/script.js
@@ -0,0 +1,21 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function incrementScore(){
+
+}
+incrementWrongAnswer(){
+
+}
+displayAdditionQuestion(){
+
+}
+displaySubtractQuestion(){
+
+}
+displayMultiplyQuestion(){
+    
+}
