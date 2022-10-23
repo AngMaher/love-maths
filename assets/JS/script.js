6b2c9c5f8f714bd9013b4e196e98c8b90699fcda
AngMaher <angelamaher@mail.com>
2022-10-23 14:36:33 +0000

    Add favicon, and final tidy ups for the user

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,6 +11,11 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
+    document.getElementById("answer-box").addEventListener("keydown",function(event){
+        if (event.key ==="Enter"){
+            checkAnswer();
+        }
+    })
 
     runGame("addition");
 })
@@ -19,8 +24,12 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType){
-    let num1 = Math.floor(Math.random() *25 + 1);
-    let num2 = Math.floor(Math.random() *25 + 1);
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+    
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
@@ -95,8 +104,8 @@ function displayAdditionQuestion(operand1, operand2){
 }
 function displaySubtractQuestion(operand1, operand2) {
 
-    document,getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document,getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 
 }
